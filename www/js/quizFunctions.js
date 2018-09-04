71a655abca77378df3eb33baf01211fef12c46c0
KristapsNaglis <35171774+KristapsNaglis@users.noreply.github.com>
2018-09-04 12:01:31 +0300

    added comments to the code

--- www/js/quizFunctions.js
+++ www/js/quizFunctions.js
@@ -1,12 +1,18 @@
+//Create variable for holding percentage
 var P;
 
+//Fill progressbar accordingly to what the percentage is
 $(document).ready(function () {
     progressBarFill();
 });
 
 //Check to see if answer is selected
 function validateForm() {
+    
+    //Create variable for holding value of form - #Form, and input - option
     var option = document.forms["Form"]["option"].value;
+
+    //If statement to check if any of radio buttons are checked
     if (option == null || option === "") {
         alert("Please Fill All Required Fields");
         return false;
@@ -15,16 +21,21 @@ function validateForm() {
 
 //Calculate percentage for progressbar
 function calculatePercentage() {
-    P = currentQuestion / totalQuestions;
-    P = P * 100;
 
+    //Formula for calculating percentage
+    P = (currentQuestion / totalQuestions) * 100;
+
+    //Return percentage variable
     return P;
 }
 
 //Fill progressbar accordingly to percentage
 function progressBarFill() {
+
+    //Create variable for progressbar - #progress
     var progress = $("#progress");
 
+    //Change the value of progressbar accordingly to the percentage
     progress.attr("value", calculatePercentage())
 }
 
